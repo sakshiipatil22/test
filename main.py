339069c205d9fb789aa6fbac5b5090f7b6d1ec65
sakshiipatil22 <psakshid24@gmail.com>
2025-01-04 13:29:21 +0530

    fixes #6: updated with separate json list

--- main.py
+++ main.py
@@ -6,19 +6,35 @@ app = FastAPI()
 @app.get("/hello")
 def hello():
     return "Bye"
+    
+data = {
+    "Sakshi": {
+        "name": "Sakshi",
+        "number": "8917273212",
+        "dob": "22/02/2004"
+    },
+    "Rahul": {
+        "name": "Rahul",
+        "number": "9087221322",
+        "dob": "10/05/2005"
+    },
+    "Sejal": {
+        "name": "Sejal",
+        "number": "8989775754",
+        "dob": "21/04/2000"
+    },
+    "Aditya": {
+        "name": "Aditya",
+        "number": "9088743212",
+        "dob": "10/11/1998"
+    }
+}
 
 @app.get("/get-name")
-def get_name(name: str):
-    
-    data = {
-        "sakshi": {"name": "Sakshi", "number": "9637271127"},
-        "rahul": {"name": "Rahul", "number": "7890123456"},
-        "ananya": {"name": "Ananya", "number": "9876543210"},
-        "arjun": {"name": "Arjun", "number": "4567891230"},
-    }
-
-    response = data.get(name.lower(), {"error": "Name not found"})  
+def get_name(name:str):
+    response = data.get(name.capitalize(), {"Error": "Name not found"})
     return response
+    
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=1000, reload=True)
