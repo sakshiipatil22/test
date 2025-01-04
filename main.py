dada6be1e0248cedd94388e9282f6237fc1d4b16
sakshiipatil22 <psakshid24@gmail.com>
2025-01-04 10:11:13 +0530

    fixes #6: added get-name route

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
-# Imports
 from fastapi import FastAPI
 import uvicorn
 
-# Create a FastAPI instance
 app = FastAPI()
 
-# Define the /hello endpoint
 @app.get("/hello")
 def hello():
     return "Bye"
 
-# Run the application when the script is executed directly
+@app.get("/get-name")
+def get_name(name: str):
+    
+    data = {
+        "sakshi": {"name": "Sakshi", "number": "9637271127"},
+        "rahul": {"name": "Rahul", "number": "7890123456"},
+        "ananya": {"name": "Ananya", "number": "9876543210"},
+        "arjun": {"name": "Arjun", "number": "4567891230"},
+    }
+
+    response = data.get(name.lower(), {"error": "Name not found"})  
+    return response
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=1000, reload=True)
