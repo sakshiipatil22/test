a0dd81691041a682d6152ca34379415b3d5d6a66
sakshiipatil22 <psakshid24@gmail.com>
2025-01-06 11:56:56 +0530

    fixes #6: changed json list format

--- main.py
+++ main.py
@@ -3,38 +3,44 @@ import uvicorn
 
 app = FastAPI()
 
+data = [
+    {
+        "name": "Sakshi",
+        "address": "xkbfdkn",
+        "dob": "22/02/2004",
+        "number": "8917273212",
+    },
+    {
+        "name": "Rahul",
+        "address": "xkbfdkn",
+        "dob": "10/05/2005",
+        "number": "9087221322",
+    },
+    {
+        "name": "Sejal",
+        "address": "xkbfdkn",
+        "dob": "21/04/2000",
+        "number": "8989775754",
+    },
+    {
+        "name": "Aditya",
+        "address": "xkbfdkn",
+        "dob": "10/11/1998",
+        "number": "9088743212",
+    },
+]
+
 @app.get("/hello")
 def hello():
     return "Bye"
-    
-data = {
-    "Sakshi": {
-        "name": "Sakshi",
-        "number": "8917273212",
-        "dob": "22/02/2004"
-    },
-    "Rahul": {
-        "name": "Rahul",
-        "number": "9087221322",
-        "dob": "10/05/2005"
-    },
-    "Sejal": {
-        "name": "Sejal",
-        "number": "8989775754",
-        "dob": "21/04/2000"
-    },
-    "Aditya": {
-        "name": "Aditya",
-        "number": "9088743212",
-        "dob": "10/11/1998"
-    }
-}
 
 @app.get("/get-name")
-def get_name(name:str):
-    response = data.get(name.capitalize(), {"Error": "Name not found"})
-    return response
-    
+def get_name(name: str):
+    for person in data:
+        if person["name"].lower() == name.lower():
+            return person
+    return {"error": "Name not found"}
 
-if __name__ == "__main__":
+
+if _name_ == "_main_":
     uvicorn.run("main:app", host="127.0.0.1", port=1000, reload=True)
