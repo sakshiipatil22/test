c417aa110f4e982292ddf08c685e30d4ba1af717
Sakshi Patil <you@example.com>
2025-01-08 16:52:26 +0530

    #11 updated lists&dicts

--- lists&dicts/key_indices_values_elements.py
+++ lists&dicts/key_indices_values_elements.py
@@ -1,1 +1,13 @@
 #Write a Python program to create a dictionary from a list where keys are indices and values are elements.
+#Output: 
+# Enter the elements in the list:sakshi, aditya, aditi, priya, rahul
+#List is : ['sakshi,', 'aditya,', 'aditi,', 'priya,', 'rahul']
+#{0: 'sakshi,', 1: 'aditya,', 2: 'aditi,', 3: 'priya,', 4: 'rahul'}
+
+
+a=str(input("Enter the elements in the list:")).split()
+print("List is :",a)
+
+
+dictt = dict(enumerate(a))
+print(dictt)
--- lists&dicts/update_value.py
+++ lists&dicts/update_value.py
@@ -1,1 +1,11 @@
 #Create a Python program to update the value of a dictionary if its key exists in a given list.
+
+test_dict = {'gfg' : [1, 5, 6], 'is' : 2, 'best' : 3}
+
+print("The original dictionary : " + str(test_dict))
+
+temp={'gfg':[x * 2 for x in test_dict['gfg']]}
+test_dict.update(temp)
+	
+
+print("Dictionary after updation is : " + str(test_dict))
